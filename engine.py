314cad246fedd93fb4343ef248364832f74dd85c
mowabanga <itsmundia@gmail.com>
2023-01-05 22:35:15 +0300

    Graphics engine

---
+++ engine.py
@@ -0,0 +1,12 @@
+import pygame as pg
+import moderngl as mgl
+import sys
+
+class graphicsEngine :
+    def __init__(self, win_size=(1600, 900)):
+        """init pygame modules
+        """
+        pg.init()
+        #window size
+        self.WIN_SIZE = win_size
+        
